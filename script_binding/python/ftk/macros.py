58fd57b075d532d0a43b975199feba468411cb09
htbegin <hotforest@gmail.com>
2011-07-19 23:23:48 +0800

    add module macros which is used to get the values of macro definitions

---
+++ script_binding/python/ftk/macros.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id: $'
+
+# macro definitions
+
+class _FtkMacroDef(object):
+    def __init__(self):
+        self._USE_STD_MALLOC = 0
+        self._FTK_COLOR_RGBA = 0
+
+    @property
+    def USE_STD_MALLOC(self):
+        return self._USE_STD_MALLOC
+
+    @property
+    def FTK_COLOR_RGBA(self):
+        return self._FTK_COLOR_RGBA
+
+ftk_macros = _FtkMacroDef()
---
+++ script_binding/python/test/test_ftk_macros.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import unittest
+
+from ftk.macros import ftk_macros
+
+class TestFtkMacros(unittest.TestCase):
+    def test_USE_STD_MALLOC(self):
+        self.assertTrue(ftk_macros.USE_STD_MALLOC in (0, 1))
+        self.assertRaises(AttributeError, setattr,
+                ftk_macros, "USE_STD_MALLOC", ftk_macros.USE_STD_MALLOC)
+
+    def test_FTK_COLOR_RGBA(self):
+        self.assertTrue(ftk_macros.FTK_COLOR_RGBA in (0, 1))
+        self.assertRaises(AttributeError, setattr,
+                ftk_macros, "FTK_COLOR_RGBA", ftk_macros.FTK_COLOR_RGBA)
+
+if __name__ == "__main__":
+    unittest.main()
