6054f6e2c71b8d3960f599f1ceda6e60873b81fd
htbegin <hotforest@gmail.com>
2011-07-24 14:57:05 +0800

    store the original source object in sources_manager object, not globally any more

--- script_binding/python/ftk/sources_manager.py
+++ script_binding/python/ftk/sources_manager.py
@@ -21,15 +21,17 @@ class FtkSourcesManager(Structure):
 _FtkSourcesManagerPtr = POINTER(FtkSourcesManager)
 _FtkSourcePtr = POINTER(ftk.source.FtkSource)
 
-ftk_sources_manager_create = ftk.dll.function('ftk_sources_manager_create',
-        '',
-        args=['max_source_nr'],
+_ftk_sources_manager_create = ftk.dll.private_function(
+        'ftk_sources_manager_create',
         arg_types=[c_int],
         return_type=_FtkSourcesManagerPtr,
         dereference_return=True,
         require_return=True)
 
-_original_src_list = []
+def ftk_sources_manager_create(max_source_nr):
+    manager = _ftk_sources_manager_create(max_source_nr)
+    manager._original_src_list = []
+    return manager
 
 _ftk_sources_manager_add = ftk.dll.private_function(
         'ftk_sources_manager_add',
@@ -39,7 +41,7 @@ _ftk_sources_manager_add = ftk.dll.private_function(
 def ftk_sources_manager_add(thiz, source):
     ret = _ftk_sources_manager_add(thiz, byref(source))
     if ret == ftk.constants.RET_OK:
-        _original_src_list.append(source)
+        thiz._original_src_list.append(source)
     return ret
 
 _ftk_sources_manager_remove = ftk.dll.private_function(
@@ -51,9 +53,9 @@ _ftk_sources_manager_remove = ftk.dll.private_function(
 def ftk_sources_manager_remove(thiz, source):
     ret = _ftk_sources_manager_remove(thiz, byref(source))
     if ret == ftk.constants.RET_OK:
-        if source in _original_src_list:
-            idx = _original_src_list.index(source)
-            del _original_src_list[idx]
+        if source in thiz._original_src_list:
+            idx = thiz._original_src_list.index(source)
+            del thiz._original_src_list[idx]
     return ret
 
 ftk_sources_manager_get_count = ftk.dll.function(
@@ -64,8 +66,8 @@ ftk_sources_manager_get_count = ftk.dll.function(
         return_type=c_int)
 
 def ftk_sources_manager_get(thiz, i):
-    if i < len(_original_src_list):
-        return _original_src_list[i]
+    if i < len(thiz._original_src_list):
+        return thiz._original_src_list[i]
     else:
         return None
 
@@ -91,4 +93,4 @@ _ftk_sources_manager_destroy = ftk.dll.private_function(
 # FIXME: support remove the source action callback when destroy the source
 def ftk_sources_manager_destroy(thiz):
     _ftk_sources_manager_destroy(thiz)
-    del _original_src_list[:]
+    del thiz._original_src_list
