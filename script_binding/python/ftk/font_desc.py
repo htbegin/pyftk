90e7098705bb9c7e438dd0bd7f9d88c0cb249ad4
htbegin <hotforest@gmail.com>
2011-07-24 12:02:19 +0800

    add module font_desc

--- script_binding/python/ftk/font_desc.py
+++ script_binding/python/ftk/font_desc.py
@@ -8,8 +8,8 @@ __version__ = '$Id: $'
 
 from ctypes import *
 
+import ftk.dll
 import ftk.constants
-import ftk.typedef
 
 # ftk_font_desc.h
 
@@ -17,4 +17,80 @@ import ftk.typedef
 class FtkFontDesc(Structure):
     pass
 
-FtkFontDescPtr = POINTER(FtkFontDesc)
+_FtkFontDescPtr = POINTER(FtkFontDesc)
+
+ftk_font_desc_create = ftk.dll.function('ftk_font_desc_create',
+        '',
+        args=['font_desc'],
+        arg_types=[c_char_p],
+        return_type=_FtkFontDescPtr,
+        dereference_return=True,
+        require_return=True)
+
+ftk_font_desc_is_equal = ftk.dll.function('ftk_font_desc_is_equal',
+        '',
+        args=['thiz', 'other'],
+        arg_types=[_FtkFontDescPtr, _FtkFontDescPtr],
+        return_type=c_int)
+
+ftk_font_desc_is_bold = ftk.dll.function('ftk_font_desc_is_bold',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkFontDescPtr],
+        return_type=c_int)
+
+ftk_font_desc_is_italic = ftk.dll.function('ftk_font_desc_is_italic',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkFontDescPtr],
+        return_type=c_int)
+
+ftk_font_desc_get_size = ftk.dll.function('ftk_font_desc_get_size',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkFontDescPtr],
+        return_type=c_int)
+
+ftk_font_desc_set_bold = ftk.dll.function('ftk_font_desc_set_bold',
+        '',
+        args=['thiz', 'bold'],
+        arg_types=[_FtkFontDescPtr, c_int],
+        return_type=c_int)
+
+ftk_font_desc_set_italic = ftk.dll.function('ftk_font_desc_set_italic',
+        '',
+        args=['thiz', 'italic'],
+        arg_types=[_FtkFontDescPtr, c_int],
+        return_type=c_int)
+
+ftk_font_desc_set_size = ftk.dll.function('ftk_font_desc_set_size',
+        '',
+        args=['thiz', 'size'],
+        arg_types=[_FtkFontDescPtr, c_int],
+        return_type=c_int)
+
+_ftk_font_desc_get_string = ftk.dll.private_function(
+        'ftk_font_desc_get_string',
+        arg_types=[_FtkFontDescPtr, c_char_p, c_uint],
+        return_type=c_int)
+
+def ftk_font_desc_get_string(thiz):
+    FONT_DESC_LEN = 64
+    desc = create_string_buffer(FONT_DESC_LEN)
+    ret = _ftk_font_desc_get_string(thiz, desc, sizeof(desc))
+    if ret == ftk.constants.RET_OK:
+        return desc.value
+    else:
+        return ""
+
+ftk_font_desc_ref = ftk.dll.function('ftk_font_desc_ref',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkFontDescPtr],
+        return_type=c_int)
+
+ftk_font_desc_unref = ftk.dll.function('ftk_font_desc_unref',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkFontDescPtr],
+        return_type=c_int)
