dea0f8593513bfd65608b2d614ecf97d119e9209
htbegin <hotforest@gmail.com>
2011-07-26 11:16:10 +0800

    add module text_layout

--- script_binding/python/ftk/text_layout.py
+++ script_binding/python/ftk/text_layout.py
@@ -26,3 +26,64 @@ class FtkTextLine(Structure):
             ('attr', c_int)
             ]
 
+_FtkTextLayoutPtr = POINTER(FtkTextLayout)
+
+_FtkFontPtr = POINTER(ftk.font.FtkFont)
+
+ftk_text_layout_create = ftk.dll.function('ftk_text_layout_create',
+        '',
+        args=[],
+        arg_types=[],
+        return_type=_FtkTextLayoutPtr,
+        dereference_return=True,
+        require_return=True)
+
+ftk_text_layout_set_font = ftk.dll.function('ftk_text_layout_set_font',
+        '',
+        args=['thiz', 'font'],
+        arg_types=[_FtkTextLayoutPtr, _FtkFontPtr],
+        return_type=c_int)
+
+ftk_text_layout_set_width = ftk.dll.function('ftk_text_layout_set_width',
+        '',
+        args=['thiz', 'width'],
+        arg_types=[_FtkTextLayoutPtr, c_uint],
+        return_type=c_int)
+
+ftk_text_layout_set_text = ftk.dll.function('ftk_text_layout_set_text',
+        '',
+        args=['thiz', 'text', 'len'],
+        arg_types=[_FtkTextLayoutPtr, c_char_p, c_int],
+        return_type=c_int)
+
+ftk_text_layout_set_wrap_mode = ftk.dll.function(
+        'ftk_text_layout_set_wrap_mode',
+        '',
+        args=['thiz', 'wrap_mode'],
+        arg_types=[_FtkTextLayoutPtr, c_int],
+        return_type=c_int)
+
+ftk_text_layout_init = ftk.dll.function('ftk_text_layout_init',
+        '',
+        args=['thiz', 'text', 'len', 'font', 'width'],
+        arg_types=[_FtkTextLayoutPtr, c_char_p, c_int, _FtkFontPtr, c_uint],
+        return_type=c_int)
+
+ftk_text_layout_skip_to = ftk.dll.function('ftk_text_layout_skip_to',
+        '',
+        args=['thiz', 'pos'],
+        arg_types=[_FtkTextLayoutPtr, c_int],
+        return_type=c_int)
+
+ftk_text_layout_get_visual_line = ftk.dll.function(
+        'ftk_text_layout_get_visual_line',
+        '',
+        args=['thiz', 'line'],
+        arg_types=[_FtkTextLayoutPtr, POINTER(FtkTextLine)],
+        return_type=c_int)
+
+ftk_text_layout_destroy = ftk.dll.function('ftk_text_layout_destroy',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkTextLayoutPtr],
+        return_type=None)
