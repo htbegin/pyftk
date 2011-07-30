70147c75ac3212c622917b70ba90f5a909bf194b
htbegin <hotforest@gmail.com>
2011-07-30 20:50:05 +0800

    add unittest for image_decoder module

---
+++ script_binding/python/test/test_ftk_image_decoder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import unittest
+
+import common
+from ftk.constants import RET_OK, RET_FAIL
+from ftk.bitmap import ftk_bitmap_unref
+from ftk.image_decoder import *
+
+class TestPngImageDecoder(unittest.TestCase):
+    def setUp(self):
+        common.setup_allocator()
+        self.png = ftk_image_png_decoder_create()
+
+    def tearDown(self):
+        ftk_image_decoder_destroy(self.png)
+
+    def test_png_decoder_create(self):
+        self.assertTrue(self.png is not None)
+
+    def test_png_decoder_match(self):
+        ret = ftk_image_decoder_match(self.png, "test.png")
+        self.assertEqual(ret, RET_OK)
+        ret = ftk_image_decoder_match(self.png, "test.jpeg")
+        self.assertEqual(ret, RET_FAIL)
+        ret = ftk_image_decoder_match(self.png, "test.bmp")
+        self.assertEqual(ret, RET_FAIL)
+
+    def test_png_decoder_decode(self):
+        bitmap = ftk_image_decoder_decode(self.png, "test.png")
+        self.assertTrue(bitmap is not None)
+        ftk_bitmap_unref(bitmap)
+
+if __name__ == "__main__":
+    unittest.main()
