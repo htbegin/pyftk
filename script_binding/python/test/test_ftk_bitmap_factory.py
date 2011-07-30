208fe1991112abb1e0cdc0ef3c13e97b367c97e9
htbegin <hotforest@gmail.com>
2011-07-30 20:59:15 +0800

    add unittest for bitmap_factory module

---
+++ script_binding/python/test/test_ftk_bitmap_factory.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import unittest
+
+import common
+from ftk.bitmap import *
+from ftk.image_decoder import *
+from ftk.bitmap_factory import *
+
+class TestBitmapFactory(unittest.TestCase):
+    def setUp(self):
+        common.setup_allocator()
+        common.setup_config()
+        self.factory = ftk_bitmap_factory_create()
+        self.png = ftk_image_png_decoder_create()
+        ftk_bitmap_factory_add_decoder(self.factory, self.png)
+
+    def tearDown(self):
+        ftk_bitmap_factory_destroy(self.factory)
+
+    def test_load(self):
+        bitmap = ftk_bitmap_factory_load(self.factory, "test.png")
+        self.assertTrue(bitmap is not None)
+        ftk_bitmap_unref(bitmap)
+
+if __name__ == "__main__":
+    unittest.main()
