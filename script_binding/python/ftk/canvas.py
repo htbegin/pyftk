d4f2d30058473065d29c02f658082dceedf24820
htbegin <hotforest@gmail.com>
2011-07-31 14:14:00 +0800

    use pointer type alias to replace POINTER(type)

--- script_binding/python/ftk/canvas.py
+++ script_binding/python/ftk/canvas.py
@@ -8,8 +8,8 @@ __version__ = '$Id: $'
 
 from ctypes import *
 
-import ftk.typedef
 import ftk.constants
+import ftk.typedef
 import ftk.bitmap
 import ftk.gc
 
@@ -20,15 +20,18 @@ class FtkCanvas(Structure):
 
 _FtkCanvasPtr = POINTER(FtkCanvas)
 _FtkBitmapPtr = POINTER(ftk.bitmap.FtkBitmap)
+_FtkRegionPtr = POINTER(ftk.typedef.FtkRegion)
+_FtkPointPtr = POINTER(ftk.typedef.FtkPoint)
+_FtkRectPtr = POINTER(ftk.typedef.FtkRect)
 
 FtkCanvasSyncGc = CFUNCTYPE(c_int, _FtkCanvasPtr)
-FtkCanvasSetClip = CFUNCTYPE(c_int, _FtkCanvasPtr, POINTER(ftk.typedef.FtkRegion))
-FtkCanvasDrawPixels = CFUNCTYPE(c_int, _FtkCanvasPtr, POINTER(ftk.typedef.FtkPoint), c_uint)
+FtkCanvasSetClip = CFUNCTYPE(c_int, _FtkCanvasPtr, _FtkRegionPtr)
+FtkCanvasDrawPixels = CFUNCTYPE(c_int, _FtkCanvasPtr, _FtkPointPtr, c_uint)
 FtkCanvasDrawLine = CFUNCTYPE(c_int, _FtkCanvasPtr, c_uint, c_uint, c_uint, c_uint)
 FtkCanvasClearRect = CFUNCTYPE(c_int, _FtkCanvasPtr, c_uint, c_uint, c_uint, c_uint)
 FtkCanvasDrawRect = CFUNCTYPE(c_int, _FtkCanvasPtr, c_uint, c_uint, c_uint, c_uint, c_int, c_int)
 FtkCanvasDrawBitmap = CFUNCTYPE(c_int, _FtkCanvasPtr, _FtkBitmapPtr,
-        POINTER(ftk.typedef.FtkRect), POINTER(ftk.typedef.FtkRect), c_int)
+        _FtkRectPtr, _FtkRectPtr, c_int)
 FtkCanvasDrawString = CFUNCTYPE(c_int, _FtkCanvasPtr, c_uint, c_uint, c_char_p, c_int, c_int)
 FtkCanvasLockBuffer = CFUNCTYPE(c_int, _FtkCanvasPtr, POINTER(_FtkBitmapPtr))
 FtkCanvasUnlockBuffer = CFUNCTYPE(c_int, _FtkCanvasPtr)
@@ -48,5 +51,6 @@ FtkCanvas._fields_ = [
         ('lock_buffer', FtkCanvasLockBuffer),
         ('unlock_buffer', FtkCanvasUnlockBuffer),
         ('destroy', FtkCanvasDestroy),
+
         ('priv', c_byte * ftk.constants.ZERO_LEN_ARRAY)
         ]
