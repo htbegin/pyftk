85d32850759abd75909ff78cac42d02c054c8a82
htbegin <hotforest@gmail.com>
2011-08-01 20:29:44 +0800

    add module text_view

---
+++ script_binding/python/ftk/text_view.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id: $'
+
+from ctypes import *
+
+import ftk.dll
+import ftk.widget
+
+# ftk_text_view.h
+
+_FtkWidgetPtr = POINTER(ftk.widget.FtkWidget)
+
+ftk_text_view_create = ftk.dll.function('ftk_text_view_create',
+        '',
+        args=['parent', 'x', 'y', 'width', 'height'],
+        arg_types=[_FtkWidgetPtr, c_int, c_int, c_int, c_int],
+        return_type=_FtkWidgetPtr,
+        dereference_return=True,
+        require_return=True)
+
+ftk_text_view_get_text = ftk.dll.function('ftk_text_view_get_text',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkWidgetPtr],
+        return_type=c_char_p)
+
+ftk_text_view_set_text = ftk.dll.function('ftk_text_view_set_text',
+        '',
+        args=['thiz', 'text', 'len'],
+        arg_types=[_FtkWidgetPtr, c_char_p, c_int],
+        return_type=c_int)
+
+ftk_text_view_insert_text = ftk.dll.function('ftk_text_view_insert_text',
+        '',
+        args=['thiz', 'pos', 'text', 'len'],
+        arg_types=[_FtkWidgetPtr, c_uint, c_char_p, c_int],
+        return_type=c_int)
+
+ftk_text_view_set_readonly = ftk.dll.function('ftk_text_view_set_readonly',
+        '',
+        args=['thiz', 'readonly'],
+        arg_types=[_FtkWidgetPtr, c_int],
+        return_type=c_int)
---
+++ script_binding/python/test/test_ftk_text_view.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+from ftk.text_view import *
+
+class TestTextView(unittest.TestCase):
+    def test_set_get_text(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
