66915923b7cbda17a9f542e679423cd204398a40
htbegin <hotforest@gmail.com>
2011-08-05 15:38:09 +0800

    add module list_view

---
+++ script_binding/python/ftk/list_view.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+'''
+'''
+
+__docformat__ = 'restructuredtext'
+__version__ = '$Id: $'
+
+from ctypes import *
+
+import ftk.dll
+import ftk.typedef
+import ftk.widget
+import ftk.list_render
+import ftk.list_model
+
+# ftk_list_view.h
+
+_FtkWidgetPtr = POINTER(ftk.widget.FtkWidget)
+
+_FtkListModelPtr = POINTER(ftk.list_model.FtkListModel)
+
+ftk_list_view_create = ftk.dll.function('ftk_list_view_create',
+        '',
+        args=['parent', 'x', 'y', 'width', 'height'],
+        arg_types=[_FtkWidgetPtr, c_int, c_int, c_int, c_int],
+        return_type=_FtkWidgetPtr,
+        dereference_return=True,
+        require_return=True)
+
+ftk_list_view_init = ftk.dll.function('ftk_list_view_init',
+        '',
+        args=['thiz', 'model', 'render', 'item_height'],
+        arg_types=[_FtkWidgetPtr, _FtkListModelPtr, POINTER(ftk.list_render.FtkListRender), c_int],
+        return_type=c_int)
+
+ftk_list_view_get_selected = ftk.dll.function('ftk_list_view_get_selected',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkWidgetPtr],
+        return_type=c_int)
+
+ftk_list_view_set_cursor = ftk.dll.function('ftk_list_view_set_cursor',
+        '',
+        args=['thiz', 'current'],
+        arg_types=[_FtkWidgetPtr, c_int],
+        return_type=c_int)
+
+ftk_list_view_get_model = ftk.dll.function('ftk_list_view_get_model',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkWidgetPtr],
+        return_type=_FtkListModelPtr,
+        dereference_return=True)
+
+_ftk_list_view_set_clicked_listener = ftk.dll.private_function(
+        'ftk_list_view_set_clicked_listener',
+        arg_types=[_FtkWidgetPtr, ftk.typedef.FtkListener, c_void_p],
+        return_type=c_int)
+
+def ftk_list_view_set_clicked_listener(thiz, listener, ctx):
+    def _listener(ignored, void_ptr):
+        list_view_ptr = cast(void_ptr, _FtkWidgetPtr)
+        return listener(ctx, list_view_ptr.contents)
+
+    callback = ftk.typedef.FtkListener(_listener)
+    ret = _ftk_list_view_set_clicked_listener(thiz, callback, None)
+    if ret == ftk.constants.RET_OK:
+        thiz._listener = callback
+
+    return ret
+
+ftk_list_view_repaint_focus_item = ftk.dll.function(
+        'ftk_list_view_repaint_focus_item',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkWidgetPtr],
+        return_type=c_int)
---
+++ script_binding/python/test/test_ftk_list_view.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+from ftk.list_view import *
+
+class TestListView(unittest.TestCase):
+    def test_pass(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
