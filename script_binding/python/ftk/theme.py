a288c9883a15f67ea7d3292fd04978bb149f4692
htbegin <hotforest@gmail.com>
2011-08-05 14:43:11 +0800

    complete module theme

--- script_binding/python/ftk/theme.py
+++ script_binding/python/ftk/theme.py
@@ -9,15 +9,17 @@ __version__ = '$Id: $'
 from ctypes import *
 
 import ftk.dll
+import ftk.typedef
 import ftk.bitmap
 import ftk.font
+import ftk.animation_trigger
 
 # ftk_theme.h
 
+_FtkBitmapPtr = POINTER(ftk.bitmap.FtkBitmap)
+
 class FtkTheme(Structure):
     pass
-
-_FtkBitmapPtr = POINTER(ftk.bitmap.FtkBitmap)
 
 _FtkThemePtr = POINTER(FtkTheme)
 
@@ -45,13 +47,15 @@ ftk_theme_load_image = ftk.dll.function('ftk_theme_load_image',
         '',
         args=['thiz', 'filename'],
         arg_types=[_FtkThemePtr, c_char_p],
-        return_type=_FtkBitmapPtr)
+        return_type=_FtkBitmapPtr,
+        dereference_return=True)
 
 ftk_theme_get_bg = ftk.dll.function('ftk_theme_get_bg',
         '',
         args=['thiz', 'type', 'state'],
         arg_types=[_FtkThemePtr, c_int, c_int],
-        return_type=_FtkBitmapPtr)
+        return_type=_FtkBitmapPtr,
+        dereference_return=True)
 
 ftk_theme_get_bg_color = ftk.dll.function('ftk_theme_get_bg_color',
         '',
@@ -75,11 +79,16 @@ ftk_theme_get_font = ftk.dll.function('ftk_theme_get_font',
         '',
         args=['thiz', 'type'],
         arg_types=[_FtkThemePtr, c_int],
-        return_type=POINTER(ftk.font.FtkFont))
+        return_type=POINTER(ftk.font.FtkFont),
+        dereference_return=True)
 
-'''
-FtkAnimationTrigger * ftk_theme_get_animation_trigger(FtkTheme * thiz);
-'''
+ftk_theme_get_animation_trigger = ftk.dll.function(
+        'ftk_theme_get_animation_trigger',
+        '',
+        args=['thiz'],
+        arg_types=[_FtkThemePtr],
+        return_type=POINTER(ftk.animation_trigger.FtkAnimationTrigger),
+        dereference_return=True)
 
 ftk_theme_destroy = ftk.dll.function('ftk_theme_destroy',
         '',
