a7dde279058eaa59f10852b9d1efa29ad4753638
htbegin <hotforest@gmail.com>
2011-08-09 09:06:53 +0800

    merge module allocator_default to module allocator

--- script_binding/python/ftk/allocator.py
+++ script_binding/python/ftk/allocator.py
@@ -14,3 +14,15 @@ import ftk.dll
 
 class FtkAllocator(Structure):
     pass
+
+_FtkAllocatorPtr = POINTER(FtkAllocator)
+
+# ftk_allocator_default.h
+
+ftk_allocator_default_create = ftk.dll.function('ftk_allocator_default_create',
+        '',
+        args=[],
+        arg_types=[],
+        return_type=_FtkAllocatorPtr,
+        dereference_return=True,
+        require_return=True)
