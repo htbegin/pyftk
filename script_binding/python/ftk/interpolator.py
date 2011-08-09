0131c7bc7298e89b4ffb0afb017b77d9e1028773
htbegin <hotforest@gmail.com>
2011-08-09 10:39:30 +0800

    Revert "add python class wrapper for FtkInterpolator"
    This reverts commit 12d6c5f718396dd7fe2c9772b5934614e9d237d9.

--- script_binding/python/ftk/interpolator.py
+++ script_binding/python/ftk/interpolator.py
@@ -13,96 +13,72 @@ import ftk.constants
 
 # ftk_interpolator.h
 
-class FtkInterpolator(object):
-    def __init__(self):
-        raise NotImplementedError("FtkInterpolator is a virtual base class")
-
-    def get(self, percent):
-        raise NotImplementedError("FtkInterpolator is a virtual base class")
-
-    def destroy(self):
-        pass
-
-class FtkBuiltInInterpolator(FtkInterpolator):
-    def __init__(self, c_interpolator):
-        self._interpolator = c_interpolator
-
-    def get(self, percent):
-        if self._interpolator.get:
-            return self._interpolator.get(self._interpolator, percent)
-        else:
-            return 0
-
-    def destroy(self):
-        if self._interpolator.destroy:
-            self._interpolator.destroy(self._interpolator)
-
-class _FtkInterpolator(Structure):
+class FtkInterpolator(Structure):
     pass
 
-_FtkInterpolatorPtr = POINTER(_FtkInterpolator)
+_FtkInterpolatorPtr = POINTER(FtkInterpolator)
 
-_FtkInterpolatorGet = CFUNCTYPE(c_float, _FtkInterpolatorPtr, c_float)
+FtkInterpolatorGet = CFUNCTYPE(c_float, _FtkInterpolatorPtr, c_float)
 
-_FtkInterpolatorDestroy = CFUNCTYPE(None, _FtkInterpolatorPtr)
+FtkInterpolatorDestroy = CFUNCTYPE(None, _FtkInterpolatorPtr)
 
-_FtkInterpolator._fields_ = [
-        ('get', _FtkInterpolatorGet),
-        ('destroy', _FtkInterpolatorDestroy),
+FtkInterpolator._fields_ = [
+        ('get', FtkInterpolatorGet),
+        ('destroy', FtkInterpolatorDestroy),
         ('priv', c_byte * ftk.constants.ZERO_LEN_ARRAY)
         ]
 
-_ftk_interpolator_linear_create = ftk.dll.private_function(
+def ftk_interpolator_get(thiz, percent):
+    if thiz.get:
+        return thiz.get(thiz, percent)
+    else:
+        return 0
+
+def ftk_interpolator_destroy(thiz):
+    if thiz.destroy:
+        thiz.destroy(thiz)
+
+ftk_interpolator_linear_create = ftk.dll.function(
         'ftk_interpolator_linear_create',
+        '',
+        args=[],
         arg_types=[],
         return_type=_FtkInterpolatorPtr,
         dereference_return=True,
         require_return=True)
 
-def ftk_interpolator_linear_create():
-    c_interpolator = _ftk_interpolator_linear_create()
-    return FtkBuiltInInterpolator(c_interpolator)
-
-_ftk_interpolator_accelerate_create = ftk.dll.private_function(
+ftk_interpolator_accelerate_create = ftk.dll.function(
         'ftk_interpolator_accelerate_create',
+        '',
+        args=[],
         arg_types=[],
         return_type=_FtkInterpolatorPtr,
         dereference_return=True,
         require_return=True)
 
-def ftk_interpolator_accelerate_create():
-    c_interpolator = _ftk_interpolator_accelerate_create()
-    return FtkBuiltInInterpolator(c_interpolator)
-
-_ftk_interpolator_decelerate_create = ftk.dll.private_function(
+ftk_interpolator_decelerate_create = ftk.dll.function(
         'ftk_interpolator_decelerate_create',
+        '',
+        args=[],
         arg_types=[],
         return_type=_FtkInterpolatorPtr,
         dereference_return=True,
         require_return=True)
 
-def ftk_interpolator_decelerate_create():
-    c_interpolator = _ftk_interpolator_decelerate_create()
-    return FtkBuiltInInterpolator(c_interpolator)
-
-_ftk_interpolator_bounce_create = ftk.dll.private_function(
+ftk_interpolator_bounce_create = ftk.dll.function(
         'ftk_interpolator_bounce_create',
+        '',
+        args=[],
         arg_types=[],
         return_type=_FtkInterpolatorPtr,
         dereference_return=True,
         require_return=True)
 
-def ftk_interpolator_bounce_create():
-    c_interpolator = _ftk_interpolator_bounce_create()
-    return FtkBuiltInInterpolator(c_interpolator)
-
-_ftk_interpolator_acc_decelerate_create = ftk.dll.private_function(
+ftk_interpolator_acc_decelerate_create = ftk.dll.function(
         'ftk_interpolator_acc_decelerate_create',
+        '',
+        args=[],
         arg_types=[],
         return_type=_FtkInterpolatorPtr,
         dereference_return=True,
         require_return=True)
-
-def ftk_interpolator_acc_decelerate_create():
-    c_interpolator = _ftk_interpolator_acc_decelerate_create()
-    return FtkBuiltInInterpolator(c_interpolator)
--- script_binding/python/test/test_ftk_interpolator.py
+++ script_binding/python/test/test_ftk_interpolator.py
@@ -6,31 +6,29 @@ import common
 
 from ftk.interpolator import *
 
-class HalfIncInterpolator(FtkInterpolator):
-    def __init__(self):
-        pass
-
-    def get(self, percent):
-        return percent + percent * 0.5
-
 class TestInterpolator(unittest.TestCase):
     def _run_till_full(self, interpolator):
         percent = 0
         while True:
             if percent >= 100: break
-            f_percent = interpolator.get(percent)
+            f_percent = ftk_interpolator_get(interpolator, percent)
             percent = int(f_percent)
             percent += 10
 
     def test_linear(self):
         linear = ftk_interpolator_linear_create()
         self._run_till_full(linear)
-        linear.destroy()
+        ftk_interpolator_destroy(linear)
 
     def test_extension(self):
-        increase = HalfIncInterpolator()
+        def half_increase(thiz, percent):
+            return percent + percent * 0.5
+
+        c_half_increase = FtkInterpolatorGet(half_increase)
+        c_destroy = FtkInterpolatorDestroy()
+        increase = FtkInterpolator(c_half_increase, c_destroy)
+
         self._run_till_full(increase)
-        increase.destroy()
 
 if __name__ == "__main__":
     unittest.main()
