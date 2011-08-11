57baffe6d4f86eb291c85e7975bf2ec49fd44774
htbegin <hotforest@gmail.com>
2011-08-11 17:51:00 +0800

    add error check for module sprite

--- script_binding/python/ftk/ftk_sprite.py
+++ script_binding/python/ftk/ftk_sprite.py
@@ -51,35 +51,37 @@ ftk_sprite_show = ftk_dll.function('ftk_sprite_show',
         '',
         args=['thiz', 'show'],
         arg_types=[_FtkSpritePtr, ctypes.c_int],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_sprite_move = ftk_dll.function('ftk_sprite_move',
         '',
         args=['thiz', 'x', 'y'],
         arg_types=[_FtkSpritePtr, ctypes.c_int, ctypes.c_int],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_sprite_set_icon = ftk_dll.function('ftk_sprite_set_icon',
         '',
         args=['thiz', 'icon'],
         arg_types=[_FtkSpritePtr, ctypes.POINTER(ftk_bitmap.FtkBitmap)],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
-_listener_refs = {}
 _ftk_sprite_set_move_listener = ftk_dll.private_function(
         'ftk_sprite_set_move_listener',
         arg_types=[_FtkSpritePtr, ftk_typedef.FtkListener, ctypes.c_void_p],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
+_listener_refs = {}
 def ftk_sprite_set_move_listener(thiz, listener, ctx):
     def _listener(ignored, ignored_too):
         return listener(ctx, thiz)
 
     callback = ftk_typedef.FtkListener(_listener)
-    ret = _ftk_sprite_set_move_listener(thiz, callback, None)
-    if ret == ftk_constants.RET_OK:
-        _listener_refs[ctypes.addressof(thiz)] = callback
-    return ret
+    _ftk_sprite_set_move_listener(thiz, callback, None)
+    _listener_refs[ctypes.addressof(thiz)] = callback
 
 _ftk_sprite_destroy = ftk_dll.private_function('ftk_sprite_destroy',
         arg_types=[_FtkSpritePtr],
