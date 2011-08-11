f1808e2b0c7d6b49fcc631532fa36f0b7bb41119
htbegin <hotforest@gmail.com>
2011-08-11 17:39:36 +0800

    add error check in module tab

--- script_binding/python/ftk/ftk_tab.py
+++ script_binding/python/ftk/ftk_tab.py
@@ -30,20 +30,21 @@ ftk_tab_get_page_count = ftk_dll.function('ftk_tab_get_page_count',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_uint)
+        return_type=ctypes.c_size_t)
 
 ftk_tab_get_page = ftk_dll.function('ftk_tab_get_page',
         '',
         args=['thiz', 'index'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint],
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t],
         return_type=_FtkWidgetPtr,
         dereference_return=True)
 
 ftk_tab_remove_page = ftk_dll.function('ftk_tab_remove_page',
         '',
         args=['thiz', 'index'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint],
-        return_type=ctypes.c_int)
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t],
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_tab_add_page = ftk_dll.function('ftk_tab_add_page',
         '',
@@ -56,35 +57,38 @@ ftk_tab_get_active_page = ftk_dll.function('ftk_tab_get_active_page',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_uint)
+        return_type=ctypes.c_size_t)
 
 ftk_tab_set_active_page = ftk_dll.function('ftk_tab_set_active_page',
         '',
         args=['thiz', 'index'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint],
-        return_type=ctypes.c_int)
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t],
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_tab_set_page_text = ftk_dll.function('ftk_tab_set_page_text',
         '',
         args=['thiz', 'index', 'text'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint, ctypes.c_char_p],
-        return_type=ctypes.c_int)
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t, ctypes.c_char_p],
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_tab_set_page_icon = ftk_dll.function('ftk_tab_set_page_icon',
         '',
         args=['thiz', 'index', 'icon'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint, _FtkBitmapPtr],
-        return_type=ctypes.c_int)
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t, _FtkBitmapPtr],
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_tab_get_page_text = ftk_dll.function('ftk_tab_get_page_text',
         '',
         args=['thiz', 'index'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint],
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t],
         return_type=ctypes.c_char_p)
 
 ftk_tab_get_page_icon = ftk_dll.function('ftk_tab_get_page_icon',
         '',
         args=['thiz', 'index'],
-        arg_types=[_FtkWidgetPtr, ctypes.c_uint],
+        arg_types=[_FtkWidgetPtr, ctypes.c_size_t],
         return_type=_FtkBitmapPtr,
         dereference_return=True)
