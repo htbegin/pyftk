35f82292c2959459094adef8b24e3b2130eb20b6
htbegin <hotforest@gmail.com>
2011-08-11 17:54:01 +0800

    add error check for module scroll_bar

--- script_binding/python/ftk/ftk_scroll_bar.py
+++ script_binding/python/ftk/ftk_scroll_bar.py
@@ -29,7 +29,8 @@ ftk_scroll_bar_set_param = ftk_dll.function('ftk_scroll_bar_set_param',
         '',
         args=['thiz', 'value', 'max_value', 'page_delta'],
         arg_types=[_FtkWidgetPtr, ctypes.c_int, ctypes.c_int, ctypes.c_int],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_scroll_bar_get_value = ftk_dll.function('ftk_scroll_bar_get_value',
         '',
@@ -47,36 +48,42 @@ ftk_scroll_bar_inc = ftk_dll.function('ftk_scroll_bar_inc',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_scroll_bar_dec = ftk_dll.function('ftk_scroll_bar_dec',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_scroll_bar_pageup = ftk_dll.function('ftk_scroll_bar_pageup',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_scroll_bar_pagedown = ftk_dll.function('ftk_scroll_bar_pagedown',
         '',
         args=['thiz'],
         arg_types=[_FtkWidgetPtr],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_scroll_bar_set_value = ftk_dll.function('ftk_scroll_bar_set_value',
         '',
         args=['thiz', 'value'],
         arg_types=[_FtkWidgetPtr, ctypes.c_int],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 _ftk_scroll_bar_set_listener = ftk_dll.private_function(
         'ftk_scroll_bar_set_listener',
         arg_types=[_FtkWidgetPtr, ftk_typedef.FtkListener, ctypes.c_void_p],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 _listener_refs = {}
 def ftk_scroll_bar_set_listener(thiz, listener, ctx):
@@ -84,7 +91,5 @@ def ftk_scroll_bar_set_listener(thiz, listener, ctx):
         return listener(ctx, thiz)
 
     callback = ftk_typedef.FtkListener(_listener)
-    ret = _ftk_scroll_bar_set_listener(thiz, callback, None)
-    if ret == ftk_constants.RET_OK:
-        _listener_refs[ctypes.addressof(thiz)] = callback
-    return ret
+    _ftk_scroll_bar_set_listener(thiz, callback, None)
+    _listener_refs[ctypes.addressof(thiz)] = callback
