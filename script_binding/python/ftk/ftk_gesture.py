dce93696ac1051c425030ab7823a2bf126c3ef99
htbegin <hotforest@gmail.com>
2011-08-13 21:45:18 +0800

    and error check for module ftk_gesture and ftk_gesture_listener

--- script_binding/python/ftk/ftk_gesture.py
+++ script_binding/python/ftk/ftk_gesture.py
@@ -9,10 +9,15 @@ __version__ = '$Id: $'
 import ctypes
 
 import ftk_dll
-import ftk_event
 import ftk_gesture_listener
+import ftk_event
 
 # ftk_gesture.h
+
+_FtkGestureListenerPtr = \
+        ctypes.POINTER(ftk_gesture_listener.FtkGestureListener)
+
+_FtkEventPtr = ctypes.POINTER(ftk_event.FtkEvent)
 
 class FtkGesture(ctypes.Structure):
     pass
@@ -22,7 +27,7 @@ _FtkGesturePtr = ctypes.POINTER(FtkGesture)
 ftk_gesture_create = ftk_dll.function('ftk_gesture_create',
         '',
         args=['listener'],
-        arg_types=[ctypes.POINTER(ftk_gesture_listener.FtkGestureListener)],
+        arg_types=[_FtkGestureListenerPtr],
         return_type=_FtkGesturePtr,
         dereference_return=True,
         require_return=True)
@@ -30,8 +35,9 @@ ftk_gesture_create = ftk_dll.function('ftk_gesture_create',
 ftk_gesture_dispatch = ftk_dll.function('ftk_gesture_dispatch',
         '',
         args=['thiz', 'event'],
-        arg_types=[_FtkGesturePtr, ctypes.POINTER(ftk_event.FtkEvent)],
-        return_type=ctypes.c_int)
+        arg_types=[_FtkGesturePtr, _FtkEventPtr],
+        return_type=ctypes.c_int,
+        check_return=True)
 
 ftk_gesture_destroy = ftk_dll.function('ftk_gesture_destroy',
         '',
--- script_binding/python/ftk/ftk_gesture_listener.py
+++ script_binding/python/ftk/ftk_gesture_listener.py
@@ -8,26 +8,36 @@ __version__ = '$Id: $'
 
 import ctypes
 
-import ftk_dll
 import ftk_constants
+import ftk_util
 import ftk_event
 
 # ftk_gesture_listener.h
 
-_FtkEventPtr = ftk_event.FtkEvent
+_FtkEventPtr = ctypes.POINTER(ftk_event.FtkEvent)
 
 class FtkGestureListener(ctypes.Structure):
     pass
 
 _FtkGestureListenerPtr = ctypes.POINTER(FtkGestureListener)
 
-FtkGestureListenerOnClicked = ctypes.CFUNCTYPE(ctypes.c_int, _FtkGestureListenerPtr, _FtkEventPtr)
-FtkGestureListenerOnDblClicked = ctypes.CFUNCTYPE(ctypes.c_int, _FtkGestureListenerPtr, _FtkEventPtr)
-FtkGestureListenerOnLongPressed = ctypes.CFUNCTYPE(ctypes.c_int, _FtkGestureListenerPtr, _FtkEventPtr)
-FtkGestureListenerOnFling = ctypes.CFUNCTYPE(ctypes.c_int, _FtkGestureListenerPtr,
-        _FtkEventPtr, _FtkEventPtr, ctypes.c_int, ctypes.c_int)
-FtkGestureListenerOnScroll = ctypes.CFUNCTYPE(ctypes.c_int, _FtkGestureListenerPtr,
-        _FtkEventPtr, _FtkEventPtr, ctypes.c_int, ctypes.c_int)
+FtkGestureListenerOnFling = ctypes.CFUNCTYPE(ctypes.c_int,
+        _FtkGestureListenerPtr, _FtkEventPtr, _FtkEventPtr, ctypes.c_int,
+        ctypes.c_int)
+
+FtkGestureListenerOnScroll = ctypes.CFUNCTYPE(ctypes.c_int,
+        _FtkGestureListenerPtr, _FtkEventPtr, _FtkEventPtr, ctypes.c_int,
+        ctypes.c_int)
+
+FtkGestureListenerOnClicked = ctypes.CFUNCTYPE(ctypes.c_int,
+        _FtkGestureListenerPtr, _FtkEventPtr)
+
+FtkGestureListenerOnDblClicked = ctypes.CFUNCTYPE(ctypes.c_int,
+        _FtkGestureListenerPtr, _FtkEventPtr)
+
+FtkGestureListenerOnLongPressed = ctypes.CFUNCTYPE(ctypes.c_int,
+        _FtkGestureListenerPtr, _FtkEventPtr)
+
 FtkGestureListenerDestroy = ctypes.CFUNCTYPE(None, _FtkGestureListenerPtr)
 
 FtkGestureListener._fields_ = [
@@ -42,33 +52,38 @@ FtkGestureListener._fields_ = [
 
 def ftk_gesture_listener_on_clicked(thiz, event):
     if thiz.on_clicked:
-        return thiz.on_clicked(thiz, event)
+        ret = thiz.on_clicked(thiz, event)
     else:
-        return ftk_constants.RET_FAIL
+        ret = ftk_constants.RET_FAIL
+    ftk_util.handle_inline_func_retval(ret)
 
 def ftk_gesture_listener_on_dbl_clicked(thiz, event):
     if thiz.on_dbl_clicked:
-        return thiz.on_dbl_clicked(thiz, event)
+        ret = thiz.on_dbl_clicked(thiz, event)
     else:
-        return ftk_constants.RET_FAIL
+        ret = ftk_constants.RET_FAIL
+    ftk_util.handle_inline_func_retval(ret)
 
 def ftk_gesture_listener_on_long_pressed(thiz, event):
     if thiz.on_long_pressed:
-        return thiz.on_long_pressed(thiz, event)
+        ret = thiz.on_long_pressed(thiz, event)
     else:
-        return ftk_constants.RET_FAIL
+        ret = ftk_constants.RET_FAIL
+    ftk_util.handle_inline_func_retval(ret)
 
 def ftk_gesture_listener_on_fling(thiz, e1, e2, velocity_x, velocity_y):
     if thiz.on_fling:
-        return thiz.on_fling(thiz, e1, e2, velocity_x, velocity_y)
+        ret = thiz.on_fling(thiz, e1, e2, velocity_x, velocity_y)
     else:
-        return ftk_constants.RET_FAIL
+        ret = ftk_constants.RET_FAIL
+    ftk_util.handle_inline_func_retval(ret)
 
 def ftk_gesture_listener_on_scroll(thiz, e1, e2, distance_x, distance_y):
     if thiz.on_scroll:
-        return thiz.on_scroll(thiz, e1, e2, distance_x, distance_y)
+        ret = thiz.on_scroll(thiz, e1, e2, distance_x, distance_y)
     else:
-        return ftk_constants.RET_FAIL
+        ret = ftk_constants.RET_FAIL
+    ftk_util.handle_inline_func_retval(ret)
 
 def ftk_gesture_listener_destroy(thiz):
     if thiz.destroy:
