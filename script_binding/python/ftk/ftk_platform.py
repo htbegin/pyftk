4553201bcfb54dedff228836826d32cc54f3fd13
htbegin <hotforest@gmail.com>
2011-08-14 14:22:31 +0800

    add error check for module ftk_platform

--- script_binding/python/ftk/ftk_platform.py
+++ script_binding/python/ftk/ftk_platform.py
@@ -15,11 +15,12 @@ import ftk_util
 
 _ftk_platform_init = ftk_dll.private_function('ftk_platform_init',
         arg_types=[ctypes.c_int, ctypes.POINTER(ctypes.c_char_p)],
-        return_type=ctypes.c_int)
+        return_type=ctypes.c_int,
+        check_return=True)
 
 def ftk_platform_init(arg_seq):
     argc, argv = ftk_util.str_seq_to_c_char_p_array(arg_seq)
-    return _ftk_platform_init(argc, argv)
+    _ftk_platform_init(argc, argv)
 
 ftk_platform_deinit = ftk_dll.function('ftk_platform_deinit',
         '',
@@ -31,4 +32,4 @@ ftk_get_relative_time = ftk_dll.function('ftk_get_relative_time',
         '',
         args=[],
         arg_types=[],
-        return_type=ctypes.c_uint)
+        return_type=ctypes.c_size_t)
--- script_binding/python/test/test_ftk_platform.py
+++ script_binding/python/test/test_ftk_platform.py
@@ -6,10 +6,10 @@ from ftk.ftk_platform import *
 
 class TestFtkPlatform(unittest.TestCase):
     def test_ftk_platform_init(self):
-        self.assertEqual(ftk_platform_init(["-v", "--level", "1"]), 0)
+        ftk_platform_init(["-v", "--level", "1"])
 
     def test_ftk_platform_deinit(self):
-        self.assertEqual(ftk_platform_init(["-d", "/tmp"]), 0)
+        ftk_platform_init(["-d", "/tmp"])
         ftk_platform_deinit()
 
     def test_ftk_get_relative_time(self):
