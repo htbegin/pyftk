b70ca8ce7956ca8792e722281de9cc5d59048acf
htbegin <hotforest@gmail.com>
2011-08-15 22:09:07 +0800

    fix the duplicated creation of app shell

--- apps/shell/main.c
+++ apps/shell/main.c
@@ -14,7 +14,7 @@ Ret ftk_main(int argc, char* argv[])
 #endif
 
 	app = ftk_app_shell_create();
-	ftk_app_run(ftk_app_shell_create(), argc, argv);
+	ftk_app_run(app, argc, argv);
 
 #ifdef HAS_MAIN
 	ftk_run();
